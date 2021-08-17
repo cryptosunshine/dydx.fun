7b058e602139560821616bcc473b031e3a8f99cf
sunshouhui <shouhui.sun@gmail.com>
2021-08-17 20:39:42 +0800

    dydx update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="logo" src="./assets/1500x500.jpeg">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
+html,body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #13131c;
+  width: 100%;
+  height: 100%
+}
+.logo{
+  width: 100%;
+  position: absolute;
+  bottom: 0;
 }
 </style>
