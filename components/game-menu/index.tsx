6a38d021578c78af3bcdb82ed83e6e403a53397e
cryptosunshine <shouhui.sun@gmail.com>
2025-04-29 14:11:00 +0800

    publish

--- components/game-menu/index.tsx
+++ components/game-menu/index.tsx
@@ -287,7 +287,7 @@ export default function GameMenu({ onScoreUpdate }: GameMenuProps) {
           className="token-button"
           onClick={() => router.push("/token")}
         >
-          🪙<div > $BlockHero</div>
+          🪙<div > $BLOCKH</div>
         </div>
       </div>
     </>
